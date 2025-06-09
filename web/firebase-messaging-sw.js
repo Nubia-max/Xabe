20bb1feafcddf01148555a1aba6c0902a86d427f
Nubia-max <nubiarabo1@gmail.com>
2025-06-09 10:05:55 +0100

    web fix

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,17 +1,15 @@
 // web/firebase-messaging-sw.js
 
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "
-AIzaSyALVaUXzWOn3fP2PjsxXXkdd2UC_Om4dGc",
+  apiKey: "AIzaSyALVaUXzWOn3fP2PjsxXXkdd2UC_Om4dGc",
   authDomain: "xabe-ai.firebaseapp.com",
   projectId: "xabe-ai",
   storageBucket: "xabe-ai.firebasestorage.app",
   messagingSenderId: "428700861857",
-  appId: "
-1:428700861857:web:996ae3b1b7050392dbf7e0",
+  appId: "1:428700861857:web:996ae3b1b7050392dbf7e0",
 });
 
 const messaging = firebase.messaging();
